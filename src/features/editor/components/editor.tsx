a580248daddff2b61615b2c92e5d89fd1daf2ed8
AmaN <webdevaman2@gmail.com>
2024-11-03 12:49:45 +0530

    editor layout

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -4,6 +4,10 @@ import React, { useEffect, useRef } from "react";
 import { fabric } from "fabric";
 
 import { useEditor } from "@/features/editor/hooks/use-editor";
+import { Navbar } from "@/features/editor/components/navbar";
+import { Sidebar } from "@/features/editor/components/sidebar";
+import { Toolbar } from "@/features/editor/components/toolbar";
+import { Footer } from "./footer";
 
 const Editor = () => {
   const { init } = useEditor();
@@ -25,8 +29,16 @@ const Editor = () => {
   //  We are trying to make canvas responsive, whenever the container size changes, we need to update the canvas size
   return (
     <div className="h-full bg-muted flex flex-col ">
-      <div className="flex-1 h-full" ref={containerRef}>
-        <canvas className="border-2" ref={canvasRef} />
+      <Navbar />
+      <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
+        <Sidebar />
+        <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
+          <Toolbar />
+          <div className="flex-1 h-[calc(100%-124px)]" ref={containerRef}>
+            <canvas className="border-2" ref={canvasRef} />
+          </div>
+          <Footer />
+        </main>
       </div>
     </div>
   );
---
+++ src/features/editor/components/navbar.tsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <nav className="h-[68px] bg-white flex items-center w-full gap-x-8 border-b lg:pl-[34px]">
+      Navbar
+    </nav>
+  );
+};
